50b33767c972cb11b1a412bc13ed1206bc285b18
alexgflegel <alex.g.flegel@gmail.com>
2019-10-31 19:10:50 +1300

    Squashed 'BoutinFlegel.Authentication.Proto/' content from commit 61d848e
    git-subtree-dir: BoutinFlegel.Authentication.Proto
    git-subtree-split: 61d848e08475ff0049699c79942999808c47e26e
    
    Added a subtree for grpc auth
    roughed out an auth service.
    Added a wiki section for image
    
    added a proto service
    Modified the encounter hub for clock services and counter services

---
+++ Narrator.Api/Services/ClockService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using BoutinFlegel.Authentication.Proto;
+using Grpc.Net.Client;
+
+namespace Narrator.Services
+{
+	public class ClockService
+	{
+		private GrpcChannel Channel { get; }
+		private Clock.ClockClient ClockClient { get; }
+
+		public ClockService()
+		{
+			Channel = GrpcChannel.ForAddress("https://localhost:5001");
+			ClockClient = new Clock.ClockClient(Channel);
+		}
+
+		public DateTime GetTime()
+		{
+			var reply = ClockClient.GetTime(new Empty { });
+			var time = DateTime.FromFileTimeUtc(reply.Time.ToDateTimeOffset()
+				.ToUnixTimeMilliseconds());
+
+			return time;
+		}
+	}
+}
